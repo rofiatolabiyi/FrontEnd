50f0a92776230bb252a7e42e9a56ee8d8135f897
Rofiat Olabiyi <g00465381@atu.ie>
2026-04-17 22:45:03 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'mini-game-hub',
+  webDir: 'www'
+};
+
+export default config;
