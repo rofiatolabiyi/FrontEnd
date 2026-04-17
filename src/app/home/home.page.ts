9fa8be033d8a3725ff87f689293180d67011005d
Rofiat Olabiyi <g00465381@atu.ie>
2026-04-17 23:22:11 +0100

    Initial commit - Mini Game Hub

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,71 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import {
+  IonContent,
+  IonHeader,
+  IonTitle,
+  IonToolbar,
+  IonList,
+  IonRadioGroup,
+  IonItem,
+  IonLabel,
+  IonRadio,
+  IonButton
+} from '@ionic/angular/standalone';
+import { Router } from '@angular/router';
+import { Storage } from '@ionic/storage-angular';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  standalone: true,
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonList,
+    IonRadioGroup,
+    IonItem,
+    IonLabel,
+    IonRadio,
+    IonButton
+  ],
 })
-export class HomePage {
-  constructor() {}
+export class HomePage implements OnInit {
+  selectedGame: string = '';
+
+  constructor(
+    private storage: Storage,
+    private router: Router
+  ) {}
+
+  async ngOnInit() {
+    await this.storage.create();
+    const savedGame = await this.storage.get('lastGame');
+    if (savedGame) {
+      this.selectedGame = savedGame;
+    }
+  }
+
+  async startGame() {
+    if (!this.selectedGame) {
+      alert('Please select a game');
+      return;
+    }
+
+    await this.storage.set('lastGame', this.selectedGame);
+
+    if (this.selectedGame === 'quiz') {
+      this.router.navigate(['/quiz']);
+    } else if (this.selectedGame === 'memory') {
+      this.router.navigate(['/memory']);
+    } else if (this.selectedGame === 'guess') {
+      this.router.navigate(['/guess']);
+    }
+  }
 }
